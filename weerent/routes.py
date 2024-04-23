bce80042d3c6ecf897cbf8e144b7bd406e0fdc64
Love <mzeelovegeneral@gmail.com>
2024-04-23 17:36:12 +0100

    update flask structure

--- weerent/routes.py
+++ weerent/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, url_for
 from weerent import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Us')
