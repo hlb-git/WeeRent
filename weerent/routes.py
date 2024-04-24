d4cd753f6380c7e35bc8b64bfb57e158b5b4dae7
Love <mzeelovegeneral@gmail.com>
2024-04-24 08:54:58 +0100

    create contact rout

--- weerent/routes.py
+++ weerent/routes.py
@@ -5,20 +5,28 @@ from weerent.forms import Register, Login
 @app.route('/')
 @app.route('/home')
 def home():
+    """Home page route."""
     return render_template('home.html')
 
 @app.route('/about')
 def about():
+    """About page route."""
     return render_template('about.html', title='About Us')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    """Register page route."""
     form = Register()
     return render_template('register.html', title='Register', form=form)
 
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    """Login page route."""
     form = Login()
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/contact')
+def contact():
+    """Contact page route."""
+    return render_template('contact.html', title='Contact Us')
 
