565a1d46780b851beee36799b1a8fc6f7767e230
ghstnet <jamespeteer6363@gmail.com>
2024-05-08 13:17:39 +0100

    mac dep

---
+++ mac_dep.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pip3 install Flask
+pip3 install flask-wtf
+pip3 install flask-sqlalchemy
+pip3 install flask-bcrypt
+pip3 install email-validator
+pip3 install flask-login
+pip3 install requests
